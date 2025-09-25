f8d8df01ae329c6ec0498b780711a56823b6a057
Cemrdeniz <blackviens@outlook.com>
2025-09-26 00:15:46 +0300

    update

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,12 +1,12 @@
-import express from 'express';
 import authController from '../controllers/auth.js';
 import { validateBody } from "../validators/validateBody.js";
-import { registerUserSchema, loginUserSchema } from '../schemas/authschemas.js';
+import { registerUserSchema, loginUserSchema } from '../schemas/authSchemas.js';
+import express from 'express';
 
 const router = express.Router();
 
 router.post('/register', validateBody(registerUserSchema), authController.registerUser);
-router.post('/login', validateBody(loginUserSchema), loginUser);
+router.post('/login', validateBody(loginUserSchema), authController.loginUser);
 router.post('/refresh', authController.refreshSession);
 router.post('/logout', authController.logoutUser);
 
