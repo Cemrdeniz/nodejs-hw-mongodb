aa405df80076ea3e42bace9a22bab8e5e6320d40
Cemrdeniz <blackviens@outlook.com>
2025-09-26 00:38:21 +0300

    update

--- src/schemas/auth.js
+++ src/schemas/auth.js
@@ -7,3 +7,13 @@ export const resetPasswordSchema = Joi.object({
   token: Joi.string().required(),
   password: Joi.string().min(6).required()
 });
+export const registerUserSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required()
+});
+
+export const loginUserSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required()
+});
