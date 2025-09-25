2d0a08da7efd8fd7b5b2279dcac3f7fa0a19bbb4
Cemrdeniz <blackviens@outlook.com>
2025-09-26 00:04:39 +0300

    update-1

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,13 +1,13 @@
 import express from 'express';
-import { registerUser, loginUser, refreshSession, logoutUser } from '../controllers/auth.js';
+import authController from '../controllers/auth.js';
 import { validateBody } from "../validators/validateBody.js";
 import { registerUserSchema, loginUserSchema } from '../schemas/authschemas.js';
 
 const router = express.Router();
 
-router.post('/register', validateBody(registerUserSchema), registerUser);
-router.post('/login', validateBody(loginUserSchema), loginUser);
-router.post('/refresh', refreshSession);
-router.post('/logout', logoutUser);
+router.post('/register', validateBody(registerUserSchema), authController.registerUser);
+router.post('/login', validateBody(loginUserSchema), authController.loginUser);
+router.post('/refresh', authController.refreshSession);
+router.post('/logout', authController.logoutUser);
 
 export default router;
