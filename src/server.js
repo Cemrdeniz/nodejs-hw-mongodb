456c4da2e88bcd185edf81cd6d9a63ab94503ccd
Cemrdeniz <blackviens@outlook.com>
2025-10-01 20:41:20 +0300

    add swagger ui

--- src/server.js
+++ src/server.js
@@ -5,8 +5,11 @@ import { env } from "./utils/env.js";
 import contactsRouter from "./routers/contacts.js";
 import { notFoundHandler } from "./middlewares/notFoundHandler.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
-import authRouter from './routers/auth.js';
-import cookieParser from 'cookie-parser';
+import authRouter from "./routers/auth.js";
+import cookieParser from "cookie-parser";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+import path from "path";
 
 export const setupServer = () => {
   const app = express();
@@ -15,17 +18,25 @@ export const setupServer = () => {
   app.use(pino());
   app.use(express.json());
   app.use(cookieParser());
+
+
   app.use("/contacts", contactsRouter);
-  app.use('/auth', authRouter); // 👈 Burası önemli!
-  // Not found middleware
+  app.use("/auth", authRouter);
+
+
+  const swaggerDocument = YAML.load(path.resolve("docs/openapi.yaml"));
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+ 
   app.use(notFoundHandler);
 
-  // Error handler middleware
+
   app.use(errorHandler);
 
   const PORT = env("PORT", false) || 3000;
   app.listen(PORT, () => {
     console.log(`✅ Server is running on port ${PORT}`);
+    console.log(`📖 Swagger docs available at http://localhost:${PORT}/api-docs`);
   });
 
   return app;
